3bb1716d8f83354681d5ee7f754b9c017ecebdab
its-asif <asifiscoding@gmail.com>
2023-03-18 20:35:49 +0600

    H. Product Manager ICPC 2022 Dhaka Regional

--- 2022_ICPC_ASIA_DHAKA_REGIONAL_CONTEST/H_Product_Manager.cpp
+++ 2022_ICPC_ASIA_DHAKA_REGIONAL_CONTEST/H_Product_Manager.cpp
@@ -8,7 +8,7 @@ int main(){
         
         int n,k; cin>>n>>k;
         for(int i=0;i<n;i++){ ll x; cin>>x;}
-        ll ans =1;
+        ll ans =1; 
         for (int i = 2; i <= n; i++)  ans *= i;
         
         cout<<"Case "<<t<<": "<<ans<<endl;
